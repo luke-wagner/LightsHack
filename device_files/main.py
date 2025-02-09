bfe5ff87a2e9e5f9228205fdd293def86e8ccc47
Luke Wagner <lukedwagner01@gmail.com>
2025-02-08 20:32:42 -0800

    Update submodules: interrupts and continuous execution

--- device_files/main.py
+++ device_files/main.py
@@ -1,1 +1,10 @@
-from espinput.iotest import *
+import time
+
+while True:
+    # Continuously run the game
+    try:
+        execfile("pacman/game.py")
+    except SystemExit:
+        pass	# Don't exit the main program if game.py exits early
+
+    time.sleep(2)
